d09ab496688e03267d83e12ca7f42722c00ddc58
Rohan Shewale <shewalerohan50@gmail.com>
2023-04-28 02:09:46 +0530

    [Rohan]Add: Check if two numbers are equal or not

---
+++ ProgrammingConstructProblems/ProgrammingConstructProblems/Number.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstructProblems
+{
+    public class Number
+    {
+        public void CheckIfTwoNumbersEqualOrNot()
+        {
+            Console.WriteLine("\n**Check if numbers are equal or not**");
+            Console.Write(">>Enter First Number: ");int num_one = Convert.ToInt32(Console.ReadLine());
+            Console.Write(">>Enter Second Number: ");int num_two = Convert.ToInt32(Console.ReadLine());
+            if (num_one == num_two)
+                Console.WriteLine(">>>>Numbers are equal");
+            else
+                Console.WriteLine(">>>>Numbers are not equal");
+        }
+    }
+}
---
+++ ProgrammingConstructProblems/ProgrammingConstructProblems/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstructProblems
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\\******Programming Construct Problem******/");
+                Console.Write("\n1.Check number are equal or not\nEnter A Choice: ");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                ChooseProgram(choice);
+            }      
+        }
+        public static void ChooseProgram(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    Number number = new Number();
+                    number.CheckIfTwoNumbersEqualOrNot();
+                    break;
+                default:
+                    Main();
+                    break;
+            }
+        }
+    }
+}
