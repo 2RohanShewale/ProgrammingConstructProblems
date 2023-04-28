9515bdb2eed01073746a6efe3483b8d11e2faad7
Rohan Shewale <shewalerohan50@gmail.com>
2023-04-28 19:23:46 +0530

    [Rohan]Add: Reversing string

--- ProgrammingConstructProblems/ProgrammingConstructProblems/Program.cs
+++ ProgrammingConstructProblems/ProgrammingConstructProblems/Program.cs
@@ -15,7 +15,7 @@ namespace ProgrammingConstructProblems
             {
                 Console.WriteLine();
                 Console.WriteLine("\\******Programming Construct Problem******/");
-                Console.Write("\n1.Check number are equal or not\n2.Ever or odd\n3.Eligible for a vote\n4.Largest of Three\n6.Find Weekday\n7.Arithmatic Operations\n8.Power Of Two\n9.Sum Of Squares\n10.Factorial\n11.Flip a coin\nEnter A Choice: ");
+                Console.Write("\n1.Check number are equal or not\n2.Ever or odd\n3.Eligible for a vote\n4.Largest of Three\n6.Find Weekday\n7.Arithmatic Operations\n8.Power Of Two\n9.Sum Of Squares\n10.Factorial\n11.Flip a coin\n12.Reverse Word\nEnter A Choice: ");
 
                 int choice = Convert.ToInt32(Console.ReadLine());
                 ChooseProgram(choice);
@@ -64,6 +64,10 @@ namespace ProgrammingConstructProblems
                 case 11:
                     CoinFlip coin = new CoinFlip();
                     coin.FlipCoin();
+                    break;
+                case 12:
+                    Reverse reverse = new Reverse();
+                    reverse.ReverseWord();
                     break;
                 default:
                     Main();
---
+++ ProgrammingConstructProblems/ProgrammingConstructProblems/Reverse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstructProblems
+{
+    internal class Reverse
+    {
+        public void ReverseWord()
+        {
+            Console.WriteLine("\n**reverse The word**");
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine();
+            int wordLength = word.Length -1;
+            string reversedWord = "";
+            while (wordLength>=0)
+            {
+                reversedWord += word[wordLength];
+                wordLength--;
+            }
+            Console.WriteLine(">>>"+reversedWord);
+        }
+    }
+}
