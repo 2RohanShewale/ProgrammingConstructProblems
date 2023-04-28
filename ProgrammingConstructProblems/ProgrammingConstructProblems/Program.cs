74ba6241145ce6d3fae230154548b48027056975
Rohan Shewale <shewalerohan50@gmail.com>
2023-04-28 19:29:27 +0530

    [Rohan]Add: Sum of first 5 natural numbers

--- ProgrammingConstructProblems/ProgrammingConstructProblems/Program.cs
+++ ProgrammingConstructProblems/ProgrammingConstructProblems/Program.cs
@@ -15,7 +15,7 @@ namespace ProgrammingConstructProblems
             {
                 Console.WriteLine();
                 Console.WriteLine("\\******Programming Construct Problem******/");
-                Console.Write("\n1.Check number are equal or not\n2.Ever or odd\n3.Eligible for a vote\n4.Largest of Three\n6.Find Weekday\n7.Arithmatic Operations\n8.Power Of Two\n9.Sum Of Squares\n10.Factorial\n11.Flip a coin\n12.Reverse Word\nEnter A Choice: ");
+                Console.Write("\n1.Check number are equal or not\n2.Ever or odd\n3.Eligible for a vote\n4.Largest of Three\n6.Find Weekday\n7.Arithmatic Operations\n8.Power Of Two\n9.Sum Of Squares\n10.Factorial\n11.Flip a coin\n12.Reverse Word\n13.Sum of first 5 natrual numbers\nEnter A Choice: ");
 
                 int choice = Convert.ToInt32(Console.ReadLine());
                 ChooseProgram(choice);
@@ -68,6 +68,10 @@ namespace ProgrammingConstructProblems
                 case 12:
                     Reverse reverse = new Reverse();
                     reverse.ReverseWord();
+                    break;
+                case 13:
+                    SumOfNumbers sum = new SumOfNumbers();
+                    sum.SumOfNarualNumbers();
                     break;
                 default:
                     Main();
---
+++ ProgrammingConstructProblems/ProgrammingConstructProblems/SumOfNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstructProblems
+{
+    internal class SumOfNumbers
+    {
+        public void SumOfNarualNumbers()
+        {
+            int num = 5;
+            int sum = 0;
+            int i = 1;
+            while (i <= 5)
+            {
+                sum += i;
+                i++;
+            }
+            Console.WriteLine($"\n>>Sum of first 5 natural number : {sum} ");
+        }
+    }
+}
