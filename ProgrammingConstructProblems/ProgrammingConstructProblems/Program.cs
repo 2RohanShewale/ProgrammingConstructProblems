a2504672b36d70770ba39b54e2023f8159c64b7c
Rohan Shewale <shewalerohan50@gmail.com>
2023-04-28 08:34:36 +0530

    [Rohan]Add: Check Eligibility for voting by age

--- ProgrammingConstructProblems/ProgrammingConstructProblems/Program.cs
+++ ProgrammingConstructProblems/ProgrammingConstructProblems/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,7 @@ namespace ProgrammingConstructProblems
             {
                 Console.WriteLine();
                 Console.WriteLine("\\******Programming Construct Problem******/");
-                Console.Write("\n1.Check number are equal or not\n2.Ever or odd\nEnter A Choice: ");
+                Console.Write("\n1.Check number are equal or not\n2.Ever or odd\n3.Eligible for a vote\nEnter A Choice: ");
 
                 int choice = Convert.ToInt32(Console.ReadLine());
                 ChooseProgram(choice);
@@ -31,6 +32,10 @@ namespace ProgrammingConstructProblems
                 case 2:
                     EvenOrOdd evenOr = new EvenOrOdd();
                     evenOr.CheckIfNumberIsEvenOrOdd();
+                    break;
+                case 3:
+                    Voting vote = new Voting();
+                    vote.EligibilityForVoting();
                     break;
                 default:
                     Main();
---
+++ ProgrammingConstructProblems/ProgrammingConstructProblems/Voting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstructProblems
+{
+    public class Voting
+    {
+        public void EligibilityForVoting()
+        {
+            Console.WriteLine("\n***Check if candidate is eligable for voting***");
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age>= 18)
+                Console.WriteLine(">>You are eligible for voting");
+            else
+                Console.WriteLine(">>You are not eligible for voting");
+        }
+    }
+}
